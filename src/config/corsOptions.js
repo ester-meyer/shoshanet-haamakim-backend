13dadfe4d6b4e2db8ba058cea3ede4dcb3125fe1
ester-meyer <mindy05832@gmail.com>
2025-10-26 01:41:41 +0200

    fix cors origin

--- src/config/corsOptions.js
+++ src/config/corsOptions.js
@@ -2,6 +2,7 @@ const corsOptions = {
   origin: "https://shoshanet-haamakim-frontend.onrender.com",
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
 };
 
 module.exports = corsOptions;
