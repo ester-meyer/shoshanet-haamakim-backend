b65c26e29642140250883ae3c78b5835d1501053
ester-meyer <mindy05832@gmail.com>
2025-10-26 01:16:57 +0200

    fic cors config

--- src/config/corsOptions.js
+++ src/config/corsOptions.js
@@ -1,6 +1,5 @@
 const corsOptions = {
   origin: [process.env.FRONTEND_URL, process.env.FRONTEND_URL],
-  credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 };
